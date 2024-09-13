f92fd6bd2b1668ecbdc6317df087a846a984a503
fcoverdugo <fcoverdugoa@underpost.net>
2024-09-13 16:17:55 -0300

    update

--- bin/index.js
+++ bin/index.js
@@ -1,18 +1,23 @@
+#!node ./bin
+
 import { spawn } from 'node:child_process';
 import { loggerFactory } from '../src/server/logger.js';
+import dotenv from 'dotenv';
+import { shellCd, shellExec } from '../src/server/process.js';
+
+dotenv.config();
 
 const logger = loggerFactory(import.meta);
 
-const underpost = spawn('underpost', ['build', 'run']);
+await logger.setUpInfo();
 
-underpost.stdout.on('data', (data) => {
-  logger.info(`stdout`, data);
-});
+switch (process.argv[3]) {
+  case 'run-dev':
+    // underpost run-dev
+    shellCd(`./node_modules/underpost-pwa-api-rest-template`);
+    shellExec(`npm run dev`);
+    break;
 
-underpost.stderr.on('data', (data) => {
-  logger.error(`stderr`, data);
-});
-
-underpost.on('close', (code) => {
-  logger.info(`child process exited with code`, code);
-});
+  default:
+    break;
+}
