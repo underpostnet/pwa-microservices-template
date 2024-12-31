40cbdb620e05d56b15111c041931016990fae4b1
fcoverdugoa <fcoverdugoa@underpost.net>
2024-12-31 07:31:46 -0300

    valkey warn message

--- src/server/valkey.js
+++ src/server/valkey.js
@@ -25,7 +25,7 @@ const valkeyClientFactory = async () => {
       if (attempt === 1) {
         valkey.disconnect();
         valkeyEnabled = false;
-        logger.error('Valkey service not enabled', { valkeyEnabled });
+        logger.warn('Valkey service not enabled', { valkeyEnabled });
         return;
       }
       return 1000; // 1 second interval attempt
