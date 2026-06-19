3cc5070e8ca55292801c2dead4ba7b0e63d92c29
underpostnet <fcoverdugoa@underpost.net>
2026-06-19 21:25:46 +0000

    ci(package-pwa-microservices-template-ghpkg): ⚙️ [engine-cyberia] Remove redundant saga index and fix null-safe frames access Migrate saga generation from DeepSeek to Gemini with staged generation Move cyberia server related modules to dedicated src/cyberia directory Introduce Top-Down PCG (Semantic Reverse-Engineering) modules and cli command [engine] Refactor introduce new projects module and client-builder modules split from server [api] Introduce base cyberia-saga api / model

---
+++ src/client-builder/client-build-live.js
@@ -0,0 +1,100 @@
+/**
+ * Module for live building client-side code
+ * @module src/client-builder/client-build-live.js
+ * @namespace clientLiveBuild
+ */
+
+import fs from 'fs-extra';
+import { Config, loadConf, readConfJson } from '../server/conf.js';
+import { loggerFactory } from '../server/logger.js';
+import { buildClient } from './client-build.js';
+
+const logger = loggerFactory(import.meta);
+
+/**
+ * @function clientLiveBuild
+ * @description Initiates a live build of client-side code.
+ * @memberof clientLiveBuild
+ */
+const clientLiveBuild = async () => {
+  if (fs.existsSync(`/tmp/client.build.json`)) {
+    const deployId = process.argv[2];
+    const subConf = process.argv[3];
+    let clientId = 'default';
+    let host = 'default.net';
+    let path = '/';
+    let baseHost = `${host}${path === '/' ? '' : path}`;
+    let views;
+    let apiBaseHost;
+    let apiBaseProxyPath;
+
+    if (
+      deployId &&
+      (fs.existsSync(`./engine-private/conf/${deployId}`) || fs.existsSync(`./engine-private/replica/${deployId}`))
+    ) {
+      loadConf(deployId, subConf);
+      const confClient = readConfJson(deployId, 'client');
+      const confServer = readConfJson(deployId, 'server');
+      host = process.argv[4];
+      path = process.argv[5];
+      clientId = confServer[host][path].client;
+      views = confClient[clientId].views;
+      baseHost = `${host}${path === '/' ? '' : path}`;
+      apiBaseHost = confServer[host][path].apiBaseHost;
+      apiBaseProxyPath = confServer[host][path].apiBaseProxyPath;
+    } else {
+      views = Config.default.client[clientId].views;
+    }
+
+    logger.info('Live build config', {
+      deployId,
+      subConf,
+      host,
+      path,
+      clientId,
+      baseHost,
+      views: views.length,
+      apiBaseHost,
+      apiBaseProxyPath,
+    });
+
+    const updates = JSON.parse(fs.readFileSync(`/tmp/client.build.json`, 'utf8'));
+    const liveClientBuildPaths = [];
+    for (let srcPath of updates) {
+      srcPath = srcPath.replaceAll('/', `\\`);
+
+      const srcBuildPath = `./src${srcPath.split('src')[1].replace(/\\/g, '/')}`;
+      if (
+        srcPath.split('src')[1].startsWith(`\\client\\components`) ||
+        srcPath.split('src')[1].startsWith(`\\client\\services`)
+      ) {
+        const publicBuildPath = `./public/${baseHost}/${srcPath.split('src')[1].slice(8)}`.replace(/\\/g, '/');
+        liveClientBuildPaths.push({ srcBuildPath, publicBuildPath });
+      } else if (srcPath.split('src')[1].startsWith(`\\client\\sw`)) {
+        const publicBuildPath = `./public/${baseHost}/sw.js`;
+        liveClientBuildPaths.push({ srcBuildPath, publicBuildPath });
+      } else if (
+        srcPath.split('src')[1].startsWith(`\\client\\offline`) &&
+        srcPath.split('src')[1].startsWith(`index.js`)
+      ) {
+        const publicBuildPath = `./public/${baseHost}/offline.js`;
+        liveClientBuildPaths.push({ srcBuildPath, publicBuildPath });
+      } else if (srcPath.split('src')[1].startsWith(`\\client`) && srcPath.slice(-9) === '.index.js') {
+        for (const view of views) {
+          const publicBuildPath = `./public/${baseHost}${view.path === '/' ? '' : view.path}/${clientId}.index.js`;
+          liveClientBuildPaths.push({ srcBuildPath, publicBuildPath });
+        }
+      } else if (srcPath.split('src')[1].startsWith(`\\client\\ssr`)) {
+        for (const view of views) {
+          const publicBuildPath = `./public/${baseHost}${view.path === '/' ? '' : view.path}/index.html`;
+          liveClientBuildPaths.push({ srcBuildPath, publicBuildPath });
+        }
+      }
+    }
+    logger.info('liveClientBuildPaths', liveClientBuildPaths);
+    await buildClient({ liveClientBuildPaths, instances: [{ host, path }] });
+    fs.removeSync(`/tmp/client.build.json`);
+  }
+};
+
+export { clientLiveBuild };
---
+++ src/client-builder/ssr.js
@@ -0,0 +1,124 @@
+/**
+ * Module for managing server side rendering
+ * @module src/client-builder/ssr.js
+ * @namespace ServerSideRendering
+ */
+
+import fs from 'fs-extra';
+import vm from 'node:vm';
+
+import Underpost from '../index.js';
+
+import { srcFormatted, JSONweb } from './client-formatted.js';
+import { loggerFactory } from '../server/logger.js';
+import { getRootDirectory } from '../server/process.js';
+
+const logger = loggerFactory(import.meta);
+
+/**
+ * Creates a server-side rendering component function from a given file path.
+ * It reads the component file, formats it, and executes it in a sandboxed Node.js VM context to extract the component.
+ * @param {string} [componentPath='./src/client/ssr/RootDocument.js'] - The path to the SSR component file.
+ * @returns {Promise<Function>} A promise that resolves to the SSR component function.
+ * @memberof ServerSideRendering
+ */
+const ssrFactory = async (componentPath = `./src/client/ssr/RootDocument.js`) => {
+  const context = { SrrComponent: () => {}, npm_package_version: Underpost.version };
+  vm.createContext(context);
+  vm.runInContext(await srcFormatted(fs.readFileSync(componentPath, 'utf8')), context);
+  return context.SrrComponent;
+};
+
+/**
+ * Sanitizes an HTML string by adding a nonce to all script and style tags for Content Security Policy (CSP).
+ * The nonce is retrieved from `res.locals.nonce`.
+ * @param {object} res - The Express response object.
+ * @param {object} req - The Express request object.
+ * @param {string} html - The HTML string to sanitize.
+ * @returns {string} The sanitized HTML string with nonces.
+ * @memberof ServerSideRendering
+ */
+const sanitizeHtml = (res, req, html) => {
+  const nonce = res.locals.nonce;
+
+  return html
+    .replace(/<script(?=\s|>)/gi, `<script nonce="${nonce}"`)
+    .replace(/<style(?=\s|>)/gi, `<style nonce="${nonce}"`);
+};
+
+/**
+ * Factory function to create Express middleware for handling 404 and 500 errors.
+ * It generates server-side rendered HTML for these error pages. If static error pages exist, it redirects to them.
+ * @param {object} options - The options for creating the middleware.
+ * @param {object} options.app - The Express app instance.
+ * @param {string} options.directory - The directory for the instance's static files.
+ * @param {string} options.rootHostPath - The root path for the host's public files.
+ * @param {string} options.path - The base path for the instance.
+ * @returns {Promise<{error500: Function, error400: Function}>} A promise that resolves to an object containing the 500 and 404 error handling middleware.
+ * @memberof ServerSideRendering
+ */
+const ssrMiddlewareFactory = async ({ app, directory, rootHostPath, path }) => {
+  const Render = await ssrFactory();
+  const ssrPath = path === '/' ? path : `${path}/`;
+
+  // Build default html src for 404 and 500
+
+  const defaultHtmlSrc404 = Render({
+    title: '404 Not Found',
+    ssrPath,
+    ssrHeadComponents: '',
+    ssrBodyComponents: (await ssrFactory(`./src/client/ssr/body/404.js`))(),
+    renderPayload: {
+      apiBasePath: process.env.BASE_API,
+      version: Underpost.version,
+    },
+    renderApi: {
+      JSONweb,
+    },
+  });
+  const path404 = `${directory ? directory : `${getRootDirectory()}${rootHostPath}`}/404/index.html`;
+  const page404 = fs.existsSync(path404) ? `${path === '/' ? '' : path}/404` : undefined;
+
+  const defaultHtmlSrc500 = Render({
+    title: '500 Server Error',
+    ssrPath,
+    ssrHeadComponents: '',
+    ssrBodyComponents: (await ssrFactory(`./src/client/ssr/body/500.js`))(),
+    renderPayload: {
+      apiBasePath: process.env.BASE_API,
+      version: Underpost.version,
+    },
+    renderApi: {
+      JSONweb,
+    },
+  });
+  const path500 = `${directory ? directory : `${getRootDirectory()}${rootHostPath}`}/500/index.html`;
+  const page500 = fs.existsSync(path500) ? `${path === '/' ? '' : path}/500` : undefined;
+
+  return {
+    error500: function (err, req, res, next) {
+      logger.error(err, err.stack);
+      if (page500) return res.status(500).redirect(page500);
+      else {
+        res.set('Content-Type', 'text/html');
+        return res.status(500).send(sanitizeHtml(res, req, defaultHtmlSrc500));
+      }
+    },
+    error400: function (req, res, next) {
+      // if /<path>/home redirect to /<path>
+      const homeRedirectPath = `${path === '/' ? '' : path}/home`;
+      if (req.url.startsWith(homeRedirectPath)) {
+        const redirectUrl = req.url.replace('/home', '');
+        return res.redirect(redirectUrl.startsWith('/') ? redirectUrl : `/${redirectUrl}`);
+      }
+
+      if (page404) return res.status(404).redirect(page404);
+      else {
+        res.set('Content-Type', 'text/html');
+        return res.status(404).send(sanitizeHtml(res, req, defaultHtmlSrc404));
+      }
+    },
+  };
+};
+
+export { ssrMiddlewareFactory, ssrFactory, sanitizeHtml };
